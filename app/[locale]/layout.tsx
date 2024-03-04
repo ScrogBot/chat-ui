67881bf7508bd09774ec4547ab2bfc8ce7860f7a
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-03-04 09:22:20 -0600

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -13,9 +13,9 @@ import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
+const APP_DEFAULT_TITLE = "Clarkson Bot"
 const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_DESCRIPTION = "Clarkson Bot PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
