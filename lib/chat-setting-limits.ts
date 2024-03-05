9b9b6341768d6718d3337e859a0a57ca0455083f
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-03-04 20:55:42 -0600

    Update chat-setting-limits.ts

--- lib/chat-setting-limits.ts
+++ lib/chat-setting-limits.ts
@@ -9,7 +9,7 @@ type ChatSettingLimits = {
 
 export const CHAT_SETTING_LIMITS: Record<LLMID, ChatSettingLimits> = {
   // ANTHROPIC MODELS
-   "claude-3-sonnet-20240229": 
+   "claude-3-sonnet-20240229": {
     MIN_TEMPERATURE: 0.0,
     MAX_TEMPERATURE: 1.0,
     MAX_TOKEN_OUTPUT_LENGTH: 4096,
