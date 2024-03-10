a67547cef294cd4e87c6fb5ff75164de492aa706
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-03-10 13:30:58 -0400

    Update platformToolsUtils.ts

--- lib/platformTools/utils/platformToolsUtils.ts
+++ lib/platformTools/utils/platformToolsUtils.ts
@@ -1,8 +1,3 @@
-import { webScraperTool } from "./library/webScraperTool"
-
-// Add your tool to the list
-export const platformToolList = [webScraperTool]
-  
 import { PlatformTool } from "@/types/platformTools"
 import { platformToolList } from "../platformToolsList"
 
