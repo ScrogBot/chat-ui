e75c8401a69358af65dc789444580d467c61387a
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-04-22 17:02:18 -0500

    Update platformToolsList.ts

--- lib/platformTools/platformToolsList.ts
+++ lib/platformTools/platformToolsList.ts
@@ -2,4 +2,4 @@ import { webScraperTool } from "./library/webScraperTool"
 import { imageGeneratorTool } from "@/lib/platformTools/library/imageGenerator"
 
 // Add your tool to the list
-export const platformToolList = [webScraperTool, imageGeneratorTool]
+export const platformToolList = [webScraperTool]
