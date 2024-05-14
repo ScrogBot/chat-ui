c492578752901c597333522fc59f99644e3b547d
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-05-14 15:22:40 -0500

    Update context.tsx

--- context/context.tsx
+++ context/context.tsx
@@ -11,7 +11,7 @@ import {
 import { AssistantImage } from "@/types/images/assistant-image"
 import { VALID_ENV_KEYS } from "@/types/valid-keys"
 import { Dispatch, SetStateAction, createContext, useState } from "react"
-import { performPubMedSearch, PubMedArticle, PubMedSearchResponse } from "@/services/pubmedService"
+import { performPubMedSearch, PubMedArticle, PubMedSearchResponse } from "../pubmedService"
 
 interface ChatbotUIContext {
   // PROFILE STORE
