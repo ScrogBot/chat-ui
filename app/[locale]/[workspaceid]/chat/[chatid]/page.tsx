2e8389604f9c25c41f05d2de03b72db5f6ec20f9
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-05-15 11:14:33 -0500

    Update page.tsx

--- app/[locale]/[workspaceid]/chat/[chatid]/page.tsx
+++ app/[locale]/[workspaceid]/chat/[chatid]/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+// app/[locale]/[workspaceid]/chat/[chatid]/page.tsx
 
-import { ChatUI } from "@/components/chat/chat-ui"
+import ChatUI from "@/components/chat/chat-ui";
 
 export default function ChatIDPage() {
-  return <ChatUI />
+  return <ChatUI />;
 }
