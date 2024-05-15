67c14cb0c13dd83bca55c54b94bee5a125bb6458
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-05-15 10:42:13 -0500

    Update pubmedService.ts

--- pubmedService.ts
+++ pubmedService.ts
@@ -17,8 +17,27 @@ export interface PubMedSearchResponse {
   };
 }
 
+export interface PubMedFetchResponse {
+  result: {
+    [key: string]: {
+      uid: string;
+      title: string;
+      abstract: string;
+      source: string;
+      authors: string[];
+      pubdate: string;
+    };
+  };
+}
+
 export const performPubMedSearch = async (query: string): Promise<PubMedSearchResponse> => {
   const response = await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=${query}&usehistory=y&retmode=json`);
   const data: PubMedSearchResponse = await response.json();
+  return data;
+};
+
+export const performPubMedFetch = async (webenv: string, querykey: string): Promise<PubMedFetchResponse> => {
+  const response = await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&query_key=${querykey}&WebEnv=${webenv}&retmode=json&retmax=15&rettype=abstract`);
+  const data: PubMedFetchResponse = await response.json();
   return data;
 };
