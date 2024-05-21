c7241cea747a090a53f014c3cc1a62d973948f3d
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-05-21 11:56:49 -0500

    Update page.tsx

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -2,7 +2,7 @@
 
 import { ChatHelp } from "@/components/chat/chat-help"
 import { useChatHandler } from "@/components/chat/chat-hooks/use-chat-handler"
-import ChatInput from "@/components/chat/chat-input"
+import { ChatInput } from "@/components/chat/chat-input"
 import ChatSettings from "@/components/chat/chat-settings"
 import ChatUI from "@/components/chat/chat-ui"
 import { QuickSettings } from "@/components/chat/quick-settings"
