35d7c6d56f06683d7bf8d9971f882376e394ac72
Daren Scroggie <dscroggie@protonmail.com>
2024-05-23 09:19:33 -0500

    more

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,5 +2,8 @@ import autoprefixer from 'autoprefixer';
 import tailwindcss from 'tailwindcss';
 
 export default {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: [
+    tailwindcss, 
+    autoprefixer,
+  ],
 };
