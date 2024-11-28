c3f250475013dbf835f552c2c4995e8f05f87452
twokindswater <jongsoo.lee@samsung.com>
2024-11-28 16:34:54 +0900

    Revert "remove unnecessary sidebar (collections, assistant, tools, models)"

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -19,11 +19,11 @@ export const Sidebar: FC<SidebarProps> = ({ contentType, showSidebar }) => {
     chats,
     presets,
     prompts,
-    files
-    //collections,
-    //assistants,
-    //tools,
-    //models
+    files,
+    collections,
+    assistants,
+    tools,
+    models
   } = useContext(ChatbotUIContext)
 
   const chatFolders = folders.filter(folder => folder.type === "chats")
@@ -81,25 +81,25 @@ export const Sidebar: FC<SidebarProps> = ({ contentType, showSidebar }) => {
             case "files":
               return renderSidebarContent("files", files, filesFolders)
 
-            // case "collections":
-            //   return renderSidebarContent(
-            //     "collections",
-            //     collections,
-            //     collectionFolders
-            //   )
+            case "collections":
+              return renderSidebarContent(
+                "collections",
+                collections,
+                collectionFolders
+              )
 
-            // case "assistants":
-            //   return renderSidebarContent(
-            //     "assistants",
-            //     assistants,
-            //     assistantFolders
-            //   )
+            case "assistants":
+              return renderSidebarContent(
+                "assistants",
+                assistants,
+                assistantFolders
+              )
 
-            // case "tools":
-            //   return renderSidebarContent("tools", tools, toolFolders)
-            //
-            // case "models":
-            //   return renderSidebarContent("models", models, modelFolders)
+            case "tools":
+              return renderSidebarContent("tools", tools, toolFolders)
+
+            case "models":
+              return renderSidebarContent("models", models, modelFolders)
 
             default:
               return null
