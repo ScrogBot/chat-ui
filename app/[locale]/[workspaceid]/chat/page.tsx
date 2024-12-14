1838e215a4b7dcf1e20db7d0415803133995f9ca
twokindswater <jongsoo.lee@samsung.com>
2024-12-14 14:38:08 +0900

    update setup

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -29,7 +29,7 @@ export default function ChatPage() {
       {chatMessages.length === 0 ? (
         <div className="relative flex h-full flex-col items-center justify-center">
           <div className="top-50% left-50% -translate-x-50% -translate-y-50% absolute mb-20">
-            <Brand theme={theme === 'dark' ? 'dark' : 'light'} />
+            {/*<Brand theme={theme === 'dark' ? 'dark' : 'light'} />*/}
           </div>
 
           <div className="absolute left-2 top-2">
