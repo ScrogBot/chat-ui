354fb32e86171a5f31c86b3346d0a776bf62f561
twokindswater <jongsoo.lee@samsung.com>
2024-12-21 18:09:35 +0900

    Add department and team

--- components/setup/profile-step.tsx
+++ components/setup/profile-step.tsx
@@ -39,6 +39,9 @@ export const ProfileStep: FC<ProfileStepProps> = ({
   const [department, updateDepartment] = useState('서울대학교'); // 초기값 설정
   const [team, updateTeam] = useState('1'); // team 상태 추가
 
+  setDepartment(department); // 부모 컴포넌트로 상태 전달
+  setTeam(team); // 부모 컴포넌트로 상태 전달
+
   // Local updater for department
   const handleDepartmentChange = (newDepartment: string) => {
     updateDepartment(newDepartment); // 로컬 상태 업데이트
@@ -174,8 +177,17 @@ export const ProfileStep: FC<ProfileStepProps> = ({
             // update the department state and the department variable
             onChange={e => handleDepartmentChange(e.target.value)}
           >
+            <option value="서울대학교">경기과학기술대</option>
+            <option value="서울대학교">경상국립대</option>
             <option value="서울대학교">서울대학교</option>
-            <option value="전북대학교">전북대학교</option>
+            <option value="서울대학교">서울시립대</option>
+            <option value="서울대학교">숙명여자대</option>
+            <option value="서울대학교">숭실대</option>
+            <option value="전북대학교">전남대</option>
+            <option value="전북대학교">전주대</option>
+            <option value="전북대학교">전북대</option>
+            <option value="전북대학교">충남대</option>
+            <option value="전북대학교">한동대</option>
           </select>
         </div>
       </div>
@@ -188,6 +200,23 @@ export const ProfileStep: FC<ProfileStepProps> = ({
             <option value="1">team 1</option>
             <option value="2">team 2</option>
             <option value="3">team 3</option>
+            <option value="4">team 4</option>
+            <option value="5">team 5</option>
+            <option value="6">team 6</option>
+            <option value="7">team 7</option>
+            <option value="8">team 8</option>
+            <option value="9">team 9</option>
+            <option value="10">team 10</option>
+            <option value="11">team 11</option>
+            <option value="12">team 12</option>
+            <option value="13">team 13</option>
+            <option value="14">team 14</option>
+            <option value="15">team 15</option>
+            <option value="16">team 16</option>
+            <option value="17">team 17</option>
+            <option value="18">team 18</option>
+            <option value="19">team 19</option>
+            <option value="20">team 20</option>
           </select>
         </div>
       </div>
